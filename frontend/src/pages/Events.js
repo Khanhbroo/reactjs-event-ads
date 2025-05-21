a4432fbc2cdf1fd20d7e5b824e5cac17f81985de
Khanhbroo <khanhmathias532211@gmail.com>
2025-05-21 18:30:05 +0700

    Set json() function for building response

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -4,6 +4,11 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+
   const events = data.events;
 
   return <EventsList events={events} />;
@@ -12,10 +17,21 @@ function EventsPage() {
 export default EventsPage;
 
 export const loader = async () => {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/event");
 
   if (!response.ok) {
-    // ...
+    // return {
+    //   isError: true,
+    //   message: "Could not fetch events",
+    // };
+
+    // throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+    //   status: 500,
+    // });
+    return Response.json(
+      { message: "Could not fetch events" },
+      { status: 500 }
+    );
   } else {
     return response;
   }
