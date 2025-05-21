42687bb65bb1788b13e20db594f4274408cc7d88
Khanhbroo <khanhmathias532211@gmail.com>
2025-05-21 16:56:53 +0700

    Stored loader() functions

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -5,7 +5,18 @@ import EventsList from "../components/EventsList";
 function EventsPage() {
   const events = useLoaderData();
 
-  return <>{<EventsList events={events} />}</>;
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
+
+export const loader = async () => {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // ...
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+
 import MainNavigation from "../components/MainNavigation";
 
 const Root = () => {
